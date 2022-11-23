f55537079fdaf41ec6d13123cbe707e0473c86ad
vovazabolotny <vovazabalotny@gmail.com>
2022-11-23 20:17:26 +0200

    init

---
+++ lab_7/lab_7.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <math.h>
+#include <time.h>
+
+using namespace std;
+
+int* fillArrayByRandomDigits(int arr[], int arraySize) {
+	
+	srand(time(0));
+	for (int i = 0; i < arraySize; i++)
+	{
+		arr[i] = rand();
+	}
+	return arr;
+}
+
+void printArray(int arr[], int arraySize) {
+	for (int i = 0; i < arraySize; i++)
+	{
+		cout << arr[i] <<endl;
+	}
+}
+int main()
+{
+	int a[10];
+	int *pointerA = fillArrayByRandomDigits(a, 10);
+
+	printArray(pointerA, 10);
+}
